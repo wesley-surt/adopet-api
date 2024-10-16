8e9d98cc482f610a38cf548f551df28036adf249
wesley-surt <wesleybruno.adm@gmail.com>
2024-10-16 14:10:58 -0300

    outro teste

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,12 +5,11 @@ import checkTonken from "../authentication/token.js";
 const router = express.Router();
 
 router
-    .get("/users", UsersController.teste)
-    .get("/users/:id", checkTonken, UsersController.return)
+    .get("/users/:id", UsersController.return)
     .post("/users/exists", UsersController.exists)
     .post("/users/login", UsersController.login)
     .post("/users/register", UsersController.register)
-    .put("/users/update", checkTonken, UsersController.update)
-    .delete("/users/:id", checkTonken, UsersController.delete);
+    .put("/users/update", UsersController.update)
+    .delete("/users/:id", UsersController.delete);
 
 export default router;
